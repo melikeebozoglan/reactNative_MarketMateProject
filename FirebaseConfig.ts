e2a4b862e353a93061bd8bae58723e13f3ff55ea
Melike Bozoğlan <m.sultanbozoglan@gmail.com>
2024-10-28 20:53:25 +0300

    problem fixed

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAx9mv7yhRFAH27n58OJYP6ikQrTy5q7CM",
-  authDomain: "seng328-a0d31.firebaseapp.com",
-  projectId: "seng328-a0d31",
-  storageBucket: "seng328-a0d31.appspot.com",
-  messagingSenderId: "608152206515",
-  appId: "1:608152206515:web:f72749217ef0a01cdc84c6",
-  measurementId: "G-W9PC90XV83"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
